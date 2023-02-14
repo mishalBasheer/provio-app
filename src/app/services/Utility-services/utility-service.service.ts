34ccd8fec77ac0e51dec593a35ef61db452b19cc
Mohammed Mishal <mishal2mk2@gmail.com>
2023-02-14 12:46:02 +0530

    seperated backend from the frontend

--- src/app/services/Utility-services/utility-service.service.ts
+++ src/app/services/Utility-services/utility-service.service.ts
@@ -8,19 +8,19 @@ import { StorageServiceService } from '../Storage-services/storage-service.servi
 export class UtilityServiceService {
   
   //behavioural subject to emit the current tab position
-  private subject = new BehaviorSubject<string | null>(this._storage.getTab());
-  
+  private subject$ = new BehaviorSubject<string | null>(this._storage.getTab());
+ 
   constructor(private _storage: StorageServiceService) {}
   
   //change current tab
   onChangeSelect(tab: string) {
     //changing current tab from the local storage using Storage Service
     this._storage.setTab(tab);
-    this.subject.next(this._storage.getTab());
+    this.subject$.next(this._storage.getTab());
   }
 
   //get current tab data as observable
   onGetSelect(): Observable<string | null> {
-    return this.subject.asObservable();
+    return this.subject$.asObservable();
   }
 }
